fa35c00a0dceee664da26b5fb9bd322b50030f36
NarayanasamyR123 <narayanasamycseksrct@gmail.com>
2023-11-17 02:17:36 +0530

    studentform proj

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-var price=10;
-var product=20;
-var tax=2;
-console.log(product);
-console.log(price+tax);
+// var a=20;
+// var b=30;
+// var c=0,d=0
+// add(a,b,c,d)
+// function add(x,y,z)
+// {
+// console.log(x+y);
+// }
+// var factor="Vijay",fmovie="Gili",fplayer="Rohit"
+// favourite()
+// function favourite()
+// {
+//     console.log("Favourite Actor "+factor);
+//     console.log("Favourite Movie "+fmovie);
+//     console.log("Favourite Player "+fplayer);
+
+// }
+
+var length=10;
+var breadth=5;
+area(length,breadth)
+function area(length,breadth)
+{
+    console.log("Area : "+(length*breadth));
+}
